39f0297f01e640e3f9f07d3cceaf26acd3520fae
Gopal Kildoliya <gopalkildoliya@gmail.com>
2019-09-05 14:47:17 +0530

    server.js added

---
+++ server.js
@@ -0,0 +1,9 @@
+// Server.js for heroku
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
