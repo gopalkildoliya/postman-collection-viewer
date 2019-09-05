abb2627e909e1427459461ec0234535aeaa6586a
Gopal Kildoliya <gopalkildoliya@gmail.com>
2019-09-05 15:20:16 +0530

    Upload added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,37 @@
 <template>
+
   <div class="columns">
     <div class="column is-3">
-      <div class="field has-addons">
-        <div class="control">
-          <input class="input" type="text" v-model="url" placeholder="URL to collection.json">
-        </div>
-        <div class="control">
-          <button class="button is-info" @click="getApi">
-            Load
-          </button>
-        </div>
-      </div>
+      
       <app-sidebar :api="api"></app-sidebar>
     </div>
-    <div class="column is-9">
+    <div class="column is-9" style="padding-top:10px;">
+    <section>
+    
+      <div class="field has-addons">
+          <div class="control">
+            <input class="input" type="text" v-model="url" placeholder="URL to collection.json">
+          </div>
+          <div class="control">
+            <button class="button is-info" @click="getApi">
+              Load
+            </button>
+          </div>
+        </div>
+        <div>
+          <b-field class="file">
+              <b-upload v-model="file" @input="uploadFile">
+                  <a class="button is-primary">
+                      <b-icon icon="upload"></b-icon>
+                      <span>Upload Collection</span>
+                  </a>
+              </b-upload>
+              <span class="file-name" v-if="file">
+                  {{ file.name }}
+              </span>
+          </b-field>
+        </div>
+      </section>
       <div>
         <div v-if="api.info" class="box">
           <h3>{{api.info.name}}</h3>
@@ -44,7 +62,8 @@ export default {
   data() {
     return {
       api: {},
-      url: ''
+      url: '',
+      file: null,
     }
   },
   mounted() {
@@ -68,6 +87,26 @@ export default {
     },
     getPath(name){
       return `/#/api/${name}`;
+    },
+    uploadFile() {
+      let that = this;
+      var reader = new FileReader();
+        reader.onload = function(){
+          var text = reader.result;
+          try {
+              var data = JSON.parse(text);
+          } catch (e) {
+             console.log("Invalid Data");
+             return;
+          }
+          
+          if (data.info) {
+            that.api = data;
+          } else {
+            console.log("Invalid Data");
+          }
+        };
+        reader.readAsText(this.file);
     }
   }
 }
